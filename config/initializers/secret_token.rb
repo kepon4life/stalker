e95d37504357e761d49217414e8db9e3608fe425
raphael.fischer <raphael.fischer@heig-vd.ch>
2013-01-17 13:50:04 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StalkerBackend::Application.config.secret_token = 'da0bdf9d6ce96c077510bd5f6837d7dccf37a4a622b324e5c3b9f9b086058b2472e371ce9387d2089cd4a40678d33832cf833ee20853eb5cd17209b6e5427c0c'
