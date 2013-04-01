e8351c832a739670368d4ab57fae8e993138cb56
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-04-01 14:11:10 +0200

    + json data loading + Customization panel

--- public/frontend/js/pictures.js
+++ public/frontend/js/pictures.js
@@ -5,17 +5,17 @@ localResultP({
         "perpage":100,
         "total":"269",
         "photo":[{
-            "url": "frontend/nucleal/pics/img1.png"
+            "url": "nucleal/pics/img1.png"
         },{
-            "url": "frontend/nucleal/pics/img3.png"
+            "url": "nucleal/pics/img3.png"
         },{
-            "url": "frontend/nucleal/pics/img4.png"
+            "url": "nucleal/pics/img4.png"
         },{
-            "url": "frontend/nucleal/pics/img5.png"
+            "url": "nucleal/pics/img5.png"
         },{
-            "url": "frontend/nucleal/pics/img6.png"
+            "url": "nucleal/pics/img6.png"
         },{
-            "url": "frontend/nucleal/pics/img7.png"
+            "url": "nucleal/pics/img7.png"
         }
         //        {
         //            "url": "pics/test-whiteontransparent-1.png"
